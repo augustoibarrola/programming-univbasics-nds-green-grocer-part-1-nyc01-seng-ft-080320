7fc79eb628eb3278bd118751c90325a24562b62f
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-31 18:59:45 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,7 +3,7 @@ require 'pry'
 def find_item_by_name_in_collection(name, collection)
   collection.each do |element| # =representative of the item hash
     element.each do |key, value| # =inside the hash itself
-      if value == name? 
+      if value == name?
         puts element
       end
     end
