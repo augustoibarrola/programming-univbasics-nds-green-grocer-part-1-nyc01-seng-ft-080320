99c3df4454016b7e13a017b2c08fa59b377c9f28
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-31 19:18:01 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -4,9 +4,7 @@ def find_item_by_name_in_collection(name, collection) # WINE, [{:item=>"DOG FOOD
   answer = Array.new
   collection.each do |element| # =representative of the item hash
     element.each_with_index do |hash, index| # =inside the hash itself
-      if hash.include? name
-        binding.pry
-
+    
       end
     end
 
